ee8e0281c314db2735d28c281cf0af26700b700d
Denis Žoljom <denis.zoljom@gmail.com>
2020-09-15 12:02:09 +0200

    Update code examples

---
+++ tests/code-example-3.php
@@ -0,0 +1,87 @@
+<?php
+
+/**
+ * Class that registers WPCLI command initial setup of theme project.
+ *
+ * @package EightshiftLibs\Cli
+ */
+
+declare(strict_types=1);
+
+namespace EightshiftLibs\Cli;
+
+use EightshiftLibs\Blocks\BlocksCli;
+use EightshiftLibs\Cli\AbstractCli;
+use EightshiftLibs\Config\ConfigCli;
+use EightshiftLibs\Enqueue\Admin\EnqueueAdminCli;
+use EightshiftLibs\Enqueue\Blocks\EnqueueBlocksCli;
+use EightshiftLibs\Enqueue\Theme\EnqueueThemeCli;
+use EightshiftLibs\Main\MainCli;
+use EightshiftLibs\Manifest\ManifestCli;
+use EightshiftLibs\Menu\MenuCli;
+
+/**
+ * Class CliInitTheme
+ */
+class CliInitTheme extends AbstractCli
+{
+
+	/**
+	 * All classes for initial theme setup for project.
+	 */
+	public const INIT_THEME_CLASSES = [
+		BlocksCli::class,
+		EnqueueAdminCli::class,
+		EnqueueBlocksCli::class,
+		EnqueueThemeCli::class,
+		ConfigCli::class,
+		MainCli::class,
+		ManifestCli::class,
+		MenuCli::class,
+	];
+
+	/**
+	 * Get WPCLI command name
+	 *
+	 * @return string
+	 */
+	public function getCommandName(): string
+	{
+		return 'init_theme';
+	}
+
+	/**
+	 * Get WPCLI command doc.
+	 *
+	 * @return string
+	 */
+	public function getDoc(): array
+	{
+		return [
+			'shortdesc' => 'Generates initial setup for WordPress theme project.',
+		];
+	}
+
+	public function __invoke(array $args, array $assoc_args) // phpcs:ignore Squiz.Commenting.FunctionComment.Missing
+	{
+		if (!function_exists('add_action')) {
+			$this->run_reset();
+			\WP_CLI::log('--------------------------------------------------');
+		}
+
+		foreach (static::INIT_THEME_CLASSES as $item) {
+			$reflection_class = new \ReflectionClass($item);
+			$class = $reflection_class->newInstanceArgs([null]);
+
+			if (function_exists('add_action')) {
+				\WP_CLI::runcommand("{$this->command_parent_name} {$class->getCommandName()}");
+			} else {
+				\WP_CLI::runcommand("eval-file bin/cli.php {$class->getCommandName()} --skip-wordpress");
+			}
+		}
+
+		\WP_CLI::log('--------------------------------------------------');
+
+		\WP_CLI::success('All commands are finished.');
+	}
+}
