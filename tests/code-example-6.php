992290d68bc0d190573448cbfe0c40ad77049dc4
Denis Žoljom <denis.zoljom@gmail.com>
2020-09-24 18:58:11 +0200

    Add corrected example
    I need to add unit tests to these tests like in the sniff.

---
+++ tests/code-example-6.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Test
+ *
+ * @package Test
+ */
+
+ namespace A;
+
+/**
+ * Class
+ */
+class ClassName
+{
+	/**
+	 * Service array
+	 *
+	 * @var array Services
+	 */
+	private $service = [];
+
+	/**
+	 * Method name
+	 */
+	private function doSomethingPrivate()
+	{
+		return '';
+	}
+}
